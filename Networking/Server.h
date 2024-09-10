dced06390f7ad0b8a214b2e6629b2db93a06aaf2
shreyman21 <shuklashreyas@icloud.com>
2024-09-10 19:59:33 -0400

    Testing in Prog

--- Networking/Server.h
+++ Networking/Server.h
@@ -1,3 +1,6 @@
+#ifndef Server_h
+#define Server_h
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -14,7 +17,9 @@ struct Server
 
     int socket;
 
-    void (*launch)(void);
+    void (*launch)(struct Server *);
 };
 
-struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void (*launch)(void));
+struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void (*launch)(struct Server *)); // Corrected function pointer signature
+
+#endif
---
+++ Networking/test.c
@@ -0,0 +1,30 @@
+#include <Server.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void launch(struct Server *server)
+{
+    char buffer[30000] = {0};
+    printf("=============== Waiting For Connection=====================\n");
+    int adress_length = sizeof(server->address);
+    int new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&adress_length);
+    if (new_socket < 0)
+    {
+        perror("Failed to accept connection");
+        exit(EXIT_FAILURE);
+    }
+
+    read(new_socket, buffer, 30000);
+    printf("%s\n", buffer);
+    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    write(new_socket, hello, strlen(hello));
+    close(new_socket);
+}
+
+int main()
+{
+    struct Server server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 80, 10, launch);
+    server.launch(&server);
+}
